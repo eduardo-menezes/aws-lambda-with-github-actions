643d18bc5eea22f09bd6294e629ff3fa62470fce
Eduardo Menezes <eduardomenezes.engenharia@gmail.com>
2022-07-26 19:26:36 -0300

    uploading the script for aws lambda

---
+++ nasa_api.py
@@ -0,0 +1,39 @@
+# importando as bibliotecas
+import pandas as pd
+import requests
+import os
+
+# pego o token de acesso definido como variavel de ambiente
+def auth():
+  api_key = os.environ.get("API_KEY")
+  return api_key
+ 
+
+# crio a url da requisição
+def cria_url(api_key):
+  lon = -95.33
+  lat = 29.78
+  date = '2018-01-01'
+  url = f'https://api.nasa.gov/planetary/earth/assets?lon={lon}&lat={lat}&date={date}&api_key={api_key}'
+  return url
+
+# faz a requiaição
+def faz_request(url):
+  response = requests.get(url)
+  return response
+
+# executo as funções anteriores
+def main():
+  api_key = auth()
+  url = cria_url(api_key)
+  response = faz_request(url)
+  return response.json()
+
+# normalizo o json da resposta para deixar colunar
+def normaliza_dados(resp):
+  return pd.json_normalize(resp, sep='_')
+
+
+response = main()
+df = normaliza_dados(response)
+print(df)
